c940da925062c5517ebb637475339e9d570dcbaa
Rahul Kumar Jhamtani <77382243+rahulkumarjhamtani@users.noreply.github.com>
2021-06-20 12:46:00 +0530

    overloading_main

--- methodoverloading3.java
+++ methodoverloading3.java
@@ -1,18 +1,20 @@
-class addr {
-    static int add(int a, int b)
-    {
-        return a+b;
-    }
-    static double add(int a, int b)
-    {
-        return a+b;
-    }
+class overloading_main {
     public static void main(String args[])
     {
-        System.out.println(addr.add(10,20));
+        System.out.println("Main with args array");
+        overloading_main.main("abc");
+        overloading_main.main();
+    }
+    public static void main(String args)
+    {
+        System.out.println("Main with args");
+    }
+    public static void main()
+    {
+        System.out.println("Main without arguments");
     }
 }
 
-// & "C:\Program Files\AdoptOpenJDK\jdk-11.0.11.9-hotspot\bin\javac" methodoverloading3.java          for JAVA file
-// & "C:\Program Files\AdoptOpenJDK\jdk-11.0.11.9-hotspot\bin\java" addr                for CLASS
+// & "C:\Program Files\AdoptOpenJDK\jdk-11.0.11.9-hotspot\bin\javac" methodoverloading4.java          for JAVA file
+// & "C:\Program Files\AdoptOpenJDK\jdk-11.0.11.9-hotspot\bin\java" overloading_main                for CLASS
 
