bb43e09e6df2cc9ce90dcd041af5ab34afc6c6c7
Rahul Kumar Jhamtani <77382243+rahulkumarjhamtani@users.noreply.github.com>
2021-07-06 09:35:20 +0530

    new-lab

---
+++ args.java
@@ -0,0 +1,12 @@
+class args {
+    public static void main(String args[])
+    {
+        for (int i=0;i<args.length;i++)
+        {
+            System.out.print("args[" + i + "] " + args[i]);
+        }
+    }
+}
+
+// & "C:\Program Files\AdoptOpenJDK\jdk-11.0.11.9-hotspot\bin\javac" abstractclass4.java          for JAVA file
+// & "C:\Program Files\AdoptOpenJDK\jdk-11.0.11.9-hotspot\bin\java" abstractclass4                for CLASS
