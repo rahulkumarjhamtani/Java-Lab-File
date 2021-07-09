88b1f9e81ad159e3376ec2664b397fc41315e739
Rahul Kumar Jhamtani <77382243+rahulkumarjhamtani@users.noreply.github.com>
2021-07-09 13:50:12 +0530

    interface

---
+++ interface3.java
@@ -0,0 +1,28 @@
+interface printtable
+{
+    void print();
+}
+interface drawtable
+{
+    void draw();
+}
+class interface3 implements printtable,drawtable
+{
+    public void print()
+    {
+        System.out.println("Hello");
+    }
+    public void draw()
+    {
+        System.out.println("Hi");
+    }
+    public static void main(String args[])
+    {
+        interface3 obj = new interface3();
+        obj.print();
+        obj.draw();
+    }
+}
+// & "C:\Program Files\AdoptOpenJDK\jdk-11.0.11.9-hotspot\bin\javac" interface3.java          for JAVA file
+// & "C:\Program Files\AdoptOpenJDK\jdk-11.0.11.9-hotspot\bin\java" interface3               for CLASS
+
