1b76dca9373f701f3ca431649ece9cf4f391e754
Rahul Kumar Jhamtani <77382243+rahulkumarjhamtani@users.noreply.github.com>
2021-07-16 20:25:59 +0530

    exception

---
+++ exception6.java
@@ -0,0 +1,24 @@
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+public class exception6 {
+    public static void main(String args[])
+    {
+        PrintWriter pw;
+        try
+        {
+            pw = new PrintWriter("exception6.txt");
+            pw.println("saved");
+        }
+        catch(FileNotFoundException e)
+        {
+            System.out.println(e);
+        }
+        System.out.println("File saved successfully");
+
+    }
+}
+
+// & "C:\Program Files\AdoptOpenJDK\jdk-11.0.11.9-hotspot\bin\javac" exception6.java          for JAVA file
+// & "C:\Program Files\AdoptOpenJDK\jdk-11.0.11.9-hotspot\bin\java" exception6                for CLASS
+
